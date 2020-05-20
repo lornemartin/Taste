5518eaac50f44982592d197b6de437eee5982482
Lorne Martin <lorne@compassfoundation.io>
2020-05-20 06:05:27 -0400

    v72 done.

--- Taste/Controllers/MenuItemController.cs
+++ Taste/Controllers/MenuItemController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -25,20 +27,32 @@ namespace Taste.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Json(new { data = _unitOfWork.me.GetAll() });
+            return Json(new { data = _unitOfWork.MenuItem.GetAll(null,null,"Category,FoodType") });
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            var objFromDb = _unitOfWork.FoodType.GetFirstOrDefault(u => u.Id == id);
-            if(objFromDb==null)
+            try
             {
-                return Json(new { success = false, message = "Error while deleting" });
-            }
-            _unitOfWork.FoodType.Remove(objFromDb);
-            _unitOfWork.Save();
+                var objFromDb = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id == id);
+                if (objFromDb == null)
+                {
+                    return Json(new { success = false, message = "Error while deleting" });
+                }
 
+                var imagePath = Path.Combine(_hostingEnvironment.WebRootPath, objFromDb.Image.TrimStart('\\'));
+                if (System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+                _unitOfWork.MenuItem.Remove(objFromDb);
+                _unitOfWork.Save();
+            }
+            catch(Exception ex)
+            {
+                return Json(new { success = false, message = "Error while deleting." });
+            }
             return Json(new { success = true, message = "Delete Successful" });
         }
     }
