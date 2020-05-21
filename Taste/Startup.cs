63d237c09a1cdcfc9f11a4ee3e76bf9ae4d79ec8
Lorne Martin <lorne@compassfoundation.io>
2020-05-21 06:17:26 -0400

    v89 done.

--- Taste/Startup.cs
+++ Taste/Startup.cs
@@ -32,7 +32,8 @@ namespace Taste
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<IdentityUser,IdentityRole>()
+                .AddDefaultTokenProviders()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
