849c65fe0adff1dd7b658243ecfee92f3be56f37
Lorne Martin <lorne@horstwelding.com>
2020-05-29 15:47:14 -0400

    Video 158 done.

--- Taste/Pages/Customer/Cart/Index.cshtml.cs
+++ Taste/Pages/Customer/Cart/Index.cshtml.cs
@@ -28,7 +28,8 @@ namespace Taste.Pages.Customer.Cart
         {
             OrderDetailsCartVM = new OrderDetailsCart()
             {
-                OrderHeader = new Models.OrderHeader()
+                OrderHeader = new Models.OrderHeader(),
+                listCart = new List<ShoppingCart>()
             };
 
             OrderDetailsCartVM.OrderHeader.OrderTotal = 0;
@@ -36,17 +37,20 @@ namespace Taste.Pages.Customer.Cart
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
-            IEnumerable<ShoppingCart> cart = _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == claim.Value);
-
-            if(cart!=null)
+            if (claim != null)
             {
-                OrderDetailsCartVM.listCart = cart.ToList();
-            }
+                IEnumerable<ShoppingCart> cart = _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == claim.Value);
 
-            foreach(var cartList in OrderDetailsCartVM.listCart)
-            {
-                cartList.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(m => m.Id == cartList.MenuItemId);
-                OrderDetailsCartVM.OrderHeader.OrderTotal += (cartList.MenuItem.Price * cartList.Count);
+                if (cart != null)
+                {
+                    OrderDetailsCartVM.listCart = cart.ToList();
+                }
+
+                foreach (var cartList in OrderDetailsCartVM.listCart)
+                {
+                    cartList.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(m => m.Id == cartList.MenuItemId);
+                    OrderDetailsCartVM.OrderHeader.OrderTotal += (cartList.MenuItem.Price * cartList.Count);
+                }
             }
 
         }
